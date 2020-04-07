26c2f283a12ead54d0936f4fe0d9bf15e1f2df81
Rodrigo L.C <rodrigolealc@ciencias.unam.mx>
2020-04-07 17:40:11 +0100

    made main executable

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ parser.add_argument('-i', '--ipaddr', dest='ip_addr', default="82.14.199.227",
 # parser.add_argument('ip_addr', help='The IP address of the game server')
 parser.add_argument('-l', '--local', action='store_const', const=True, default=False,
                     help='Use the default local IP address (not localhost)')
-parser.add_argument('-p', '--port', type=int, default=5555)
+parser.add_argument('-p', '--port', type=int, default=5555,
+                    help='The port to use (default is 5555)')
 args = parser.parse_args()
 
 try:
